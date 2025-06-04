5aa0515d8d0bf71d7acc8ed2da60cf0edf021721
Kooler Fan <koolerkx@gmail.com>
2025-06-04 10:27:57 +0900

    WinMain entry

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+// WinMain: program entry
+int APIENTRY WinMain(
+	_In_ HINSTANCE hInstance,
+	//_In_opt_ HINSTANCE hPrevInstance,
+	_In_opt_ HINSTANCE,	// keep type only to remove the warning
+	_In_ LPSTR lpCmdLine, 
+	_In_ int nCmdShow)
+{
+	return 0;
+}
