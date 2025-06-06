cb3da6a15a3340d452b92b20138ad8c9565b3a8c
Kooler Fan <koolerkx@gmail.com>
2025-06-06 18:05:43 +0900

    sample2: direct3d initialize

--- GameSample02/main.cpp
+++ GameSample02/main.cpp
@@ -2,6 +2,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include "game_window.h"
+#include "direct3d.h"
 
 // ÉÅÉCÉì
 int APIENTRY WinMain(
@@ -11,6 +12,8 @@ int APIENTRY WinMain(
 	_In_ int nCmdShow)
 {
 	HWND hWnd = GameWindow_Create(hInstance);
+	
+	Direct3D_Initialize(hWnd);
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
@@ -21,7 +24,11 @@ int APIENTRY WinMain(
 	while (GetMessage(&msg, nullptr, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
+
+		Direct3D_Clear();
+		Direct3D_Present();
 	}
+	Direct3D_Finalize();
 
 	return (int)msg.wParam;
 }
